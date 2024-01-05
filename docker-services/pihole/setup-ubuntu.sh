1b49403c69bd456542b47d6729f2df473d6cf85e
fmcato <fmcato@gmail.com>
2024-01-05 17:07:23 +0000

    Add useful docker services

---
+++ docker-services/pihole/setup-ubuntu.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+sed -r -i.orig 's/#?DNSStubListener=yes/DNSStubListener=no/g' /etc/systemd/resolved.conf
+sh -c 'rm /etc/resolv.conf && ln -s /run/systemd/resolve/resolv.conf /etc/resolv.conf'
+systemctl restart systemctl-resolved
+
